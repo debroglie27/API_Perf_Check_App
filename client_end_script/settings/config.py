2adad1be09c7f69b136b074ea3c9b14e67daa263
Arijeet De <arijeetde@gmail.com>
2024-09-21 19:08:12 +0530

    relocating coursecode

--- client_end_script/settings/config.py
+++ client_end_script/settings/config.py
@@ -1,11 +1,14 @@
 LOG_HOST="10.129.7.11"
 TEST_SERVER_HOST="https://safev2.cse.iitb.ac.in/"
+
 SERVER_DAEMON_PORT=5000
 FTP_SERVER_PORT=5001
 SEARCH_LINES_LIMIT=200000
 
+COURSE_CODE="CS007"
+
 RESULT_PORT = 5500 # just for reference dont change it
 
 # Scalability Analysis:
-#          - Processes (5): 177
-#          - Processes (24): 112
+#          - Processes (5): 112
+#          - Processes (24): 177
