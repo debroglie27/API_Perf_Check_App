281d220d350feb4ba65c88d5f836d035b148a039
Arijeet De <arijeetde@gmail.com>
2024-12-31 11:46:53 +0530

    removed duration entry and default val for delays

--- client_end_script/utilities/launch_gui.py
+++ client_end_script/utilities/launch_gui.py
@@ -20,8 +20,19 @@ def launch_gui():
         try:
             # Collect and validate input from the GUI
             user_inputs["num_users"] = int(num_users_entry.get())
-            user_inputs["ramp_up"] = int(ramp_up_entry.get())
-            user_inputs["duration"] = int(duration_entry.get())
+            user_inputs["ramp_up"] = float(ramp_up_entry.get())
+
+            # Collect delays from the delay entry fields
+            user_inputs["delays"] = [
+                int(delay_login_entry.get()),
+                int(delay_course_list_entry.get()),
+                int(delay_quiz_list_entry.get()),
+                int(delay_quiz_info_entry.get()),
+                int(delay_quiz_download_entry.get()),
+                int(delay_quiz_authenticate_entry.get()),
+                int(delay_quiz_submit_entry.get())
+            ]
+
             root.destroy()  # Close the GUI
         except ValueError:
             print("Invalid input! Please enter valid integers.")
@@ -32,7 +43,7 @@ def launch_gui():
         sys.exit()
 
     # Store inputs in a dictionary for easy retrieval
-    user_inputs = {"num_users": None, "ramp_up": None, "duration": None}
+    user_inputs = {"num_users": None, "ramp_up": None, "delays": [1, 1, 1, 1, 1, 1, 1]}
 
     # Create the GUI window
     root = Tk()
@@ -43,7 +54,7 @@ def launch_gui():
 
     # Set desired window dimensions
     window_width = 650
-    window_height = 700
+    window_height = 690
 
     # Center the window
     center_window(root, window_width, window_height)
@@ -54,33 +65,31 @@ def launch_gui():
     # Configure the grid layout
     root.grid_columnconfigure(0, weight=1)  # Center column 0
 
-    label_font = ("Arial", 15)
+    input_label_font = ("Arial", 18)
+    delay_label_font = ("Arial", 15)
     header_font = ("Arial", 20)
-    entry_font = ("Arial", 15)
-    button_font = ("Arial", 15)
+    input_entry_font = ("Arial", 18)
+    delay_entry_font = ("Arial", 15)
+    button_font = ("Arial", 16)
 
     # Frame for Delays
     inputs_frame = Frame(root, bg="#a0dafa")
-    inputs_frame.grid(row=0, column=0, padx=10, pady=(50, 10), sticky="nsew")
+    inputs_frame.grid(row=0, column=0, padx=10, pady=(60, 20), sticky="nsew")
 
     inputs_frame.grid_columnconfigure(0, weight=1)
     inputs_frame.grid_columnconfigure(1, weight=1)
 
     # Num Users Label and Entry
-    Label(inputs_frame, text="Number of Users:", font=label_font, bg="#a0dafa").grid(row=0, column=0, padx=10, pady=(0, 10), sticky="e")
-    num_users_entry = Entry(inputs_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(inputs_frame, text="Number of Users:", font=input_label_font, bg="#a0dafa").grid(row=0, column=0, padx=10, pady=(0, 10), sticky="e")
+    num_users_entry = Entry(inputs_frame, font=input_entry_font, borderwidth=0, relief="flat")
     num_users_entry.grid(row=0, column=1, padx=10, pady=(0, 10), sticky="w")
 
-    Label(inputs_frame, text="Ramp-Up Rate:", font=label_font, bg="#a0dafa").grid(row=1, column=0, padx=10, pady=10, sticky="e")
-    ramp_up_entry = Entry(inputs_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(inputs_frame, text="Ramp-Up Rate:", font=input_label_font, bg="#a0dafa").grid(row=1, column=0, padx=10, pady=10, sticky="e")
+    ramp_up_entry = Entry(inputs_frame, font=input_entry_font, borderwidth=0, relief="flat")
     ramp_up_entry.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
-    Label(inputs_frame, text="Duration:", font=label_font, bg="#a0dafa").grid(row=2, column=0, padx=10, pady=10, sticky="e")
-    duration_entry = Entry(inputs_frame, font=entry_font, borderwidth=0, relief="flat")
-    duration_entry.grid(row=2, column=1, padx=10, pady=10, sticky="w")
-
     # Delay Label
-    Label(root, text="Delays", font=header_font, bg="#a0dafa").grid(row=1, column=0, pady=(20, 0), sticky="nsew")
+    Label(root, text="Delays (secs)", font=header_font, bg="#a0dafa").grid(row=1, column=0, pady=(20, 0), sticky="nsew")
 
     # Frame for Delays
     delays_frame = Frame(root, bg="#a0dafa")
@@ -90,40 +99,47 @@ def launch_gui():
     delays_frame.grid_columnconfigure(1, weight=1)
     delays_frame.grid_columnconfigure(2, weight=1)
 
-    Label(delays_frame, text="Login", font=label_font, bg="#a0dafa").grid(row=0, column=0, padx=10, pady=8, sticky="e")
-    delay_login_entry = Entry(delays_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(delays_frame, text="Login", font=delay_label_font, bg="#a0dafa").grid(row=0, column=0, padx=10, pady=8, sticky="e")
+    delay_login_entry = Entry(delays_frame, font=delay_entry_font, borderwidth=0, relief="flat", justify="center")
     delay_login_entry.grid(row=0, column=1, padx=10, pady=8)
-    Label(delays_frame, text="Course List", font=label_font, bg="#a0dafa").grid(row=0, column=2, padx=10, pady=8, sticky="w")
+    delay_login_entry.insert(0, "1")  # Default value
+    Label(delays_frame, text="Course List", font=delay_label_font, bg="#a0dafa").grid(row=0, column=2, padx=10, pady=8, sticky="w")
 
-    Label(delays_frame, text="Course List", font=label_font, bg="#a0dafa").grid(row=1, column=0, padx=10, pady=8, sticky="e")
-    delay_course_list_entry = Entry(delays_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(delays_frame, text="Course List", font=delay_label_font, bg="#a0dafa").grid(row=1, column=0, padx=10, pady=8, sticky="e")
+    delay_course_list_entry = Entry(delays_frame, font=delay_entry_font, borderwidth=0, relief="flat", justify="center")
     delay_course_list_entry.grid(row=1, column=1, padx=10, pady=8)
-    Label(delays_frame, text="Quiz List", font=label_font, bg="#a0dafa").grid(row=1, column=2, padx=10, pady=8, sticky="w")
+    delay_course_list_entry.insert(0, "1")  # Default value
+    Label(delays_frame, text="Quiz List", font=delay_label_font, bg="#a0dafa").grid(row=1, column=2, padx=10, pady=8, sticky="w")
 
-    Label(delays_frame, text="Quiz List", font=label_font, bg="#a0dafa").grid(row=2, column=0, padx=10, pady=8, sticky="e")
-    delay_quiz_list_entry = Entry(delays_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(delays_frame, text="Quiz List", font=delay_label_font, bg="#a0dafa").grid(row=2, column=0, padx=10, pady=8, sticky="e")
+    delay_quiz_list_entry = Entry(delays_frame, font=delay_entry_font, borderwidth=0, relief="flat", justify="center")
     delay_quiz_list_entry.grid(row=2, column=1, padx=10, pady=8)
-    Label(delays_frame, text="Quiz Info", font=label_font, bg="#a0dafa").grid(row=2, column=2, padx=10, pady=8, sticky="w")
+    delay_quiz_list_entry.insert(0, "1") # Default value
+    Label(delays_frame, text="Quiz Info", font=delay_label_font, bg="#a0dafa").grid(row=2, column=2, padx=10, pady=8, sticky="w")
 
-    Label(delays_frame, text="Quiz Info", font=label_font, bg="#a0dafa").grid(row=3, column=0, padx=10, pady=8, sticky="e")
-    delay_quiz_info_entry = Entry(delays_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(delays_frame, text="Quiz Info", font=delay_label_font, bg="#a0dafa").grid(row=3, column=0, padx=10, pady=8, sticky="e")
+    delay_quiz_info_entry = Entry(delays_frame, font=delay_entry_font, borderwidth=0, relief="flat", justify="center")
     delay_quiz_info_entry.grid(row=3, column=1, padx=10, pady=8)
-    Label(delays_frame, text="Quiz Download", font=label_font, bg="#a0dafa").grid(row=3, column=2, padx=10, pady=8, sticky="w")
+    delay_quiz_info_entry.insert(0, "1") # Default value
+    Label(delays_frame, text="Quiz Download", font=delay_label_font, bg="#a0dafa").grid(row=3, column=2, padx=10, pady=8, sticky="w")
 
-    Label(delays_frame, text="Quiz Download", font=label_font, bg="#a0dafa").grid(row=4, column=0, padx=10, pady=8, sticky="e")
-    delay_quiz_download_entry = Entry(delays_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(delays_frame, text="Quiz Download", font=delay_label_font, bg="#a0dafa").grid(row=4, column=0, padx=10, pady=8, sticky="e")
+    delay_quiz_download_entry = Entry(delays_frame, font=delay_entry_font, borderwidth=0, relief="flat", justify="center")
     delay_quiz_download_entry.grid(row=4, column=1, padx=10, pady=8)
-    Label(delays_frame, text="Quiz Authenticate", font=label_font, bg="#a0dafa").grid(row=4, column=2, padx=10, pady=8, sticky="w")
+    delay_quiz_download_entry.insert(0, "1") # Default value
+    Label(delays_frame, text="Quiz Authenticate", font=delay_label_font, bg="#a0dafa").grid(row=4, column=2, padx=10, pady=8, sticky="w")
 
-    Label(delays_frame, text="Quiz Authenticate", font=label_font, bg="#a0dafa").grid(row=5, column=0, padx=10, pady=8, sticky="e")
-    delay_quiz_authenticate_entry = Entry(delays_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(delays_frame, text="Quiz Authenticate", font=delay_label_font, bg="#a0dafa").grid(row=5, column=0, padx=10, pady=8, sticky="e")
+    delay_quiz_authenticate_entry = Entry(delays_frame, font=delay_entry_font, borderwidth=0, relief="flat", justify="center")
     delay_quiz_authenticate_entry.grid(row=5, column=1, padx=10, pady=8)
-    Label(delays_frame, text="Quiz Submit", font=label_font, bg="#a0dafa").grid(row=5, column=2, padx=10, pady=8, sticky="w")
+    delay_quiz_authenticate_entry.insert(0, "1") # Default value
+    Label(delays_frame, text="Quiz Submit", font=delay_label_font, bg="#a0dafa").grid(row=5, column=2, padx=10, pady=8, sticky="w")
 
-    Label(delays_frame, text="Quiz Submit", font=label_font, bg="#a0dafa").grid(row=6, column=0, padx=10, pady=8, sticky="e")
-    delay_quiz_submit_entry = Entry(delays_frame, font=entry_font, borderwidth=0, relief="flat")
+    Label(delays_frame, text="Quiz Submit", font=delay_label_font, bg="#a0dafa").grid(row=6, column=0, padx=10, pady=8, sticky="e")
+    delay_quiz_submit_entry = Entry(delays_frame, font=delay_entry_font, borderwidth=0, relief="flat", justify="center")
     delay_quiz_submit_entry.grid(row=6, column=1, padx=10, pady=8)
-    Label(delays_frame, text="Finish", font=label_font, bg="#a0dafa").grid(row=6, column=2, padx=10, pady=8, sticky="w")
+    delay_quiz_submit_entry.insert(0, "1") # Default value
+    Label(delays_frame, text="Finish", font=delay_label_font, bg="#a0dafa").grid(row=6, column=2, padx=10, pady=8, sticky="w")
 
 
     # Submit button to validate input and close the GUI
@@ -133,4 +149,4 @@ def launch_gui():
     root.mainloop()
 
     # Return user inputs after the GUI is closed
-    return user_inputs["num_users"], user_inputs["ramp_up"], user_inputs["duration"]
+    return user_inputs["num_users"], user_inputs["ramp_up"], user_inputs["delays"]
