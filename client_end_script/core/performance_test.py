cec2ff75d1a2f37c6db6acf264cfb78cd7c14e07
Arijeet De <arijeetde@gmail.com>
2025-01-04 21:34:30 +0530

    fixed the locust stats writing

--- client_end_script/core/performance_test.py
+++ client_end_script/core/performance_test.py
@@ -4,19 +4,42 @@ import logging
 import requests
 from math import ceil
 from locust.env import Environment
-from locust.stats import stats_printer, stats_history, StatsCSVFileWriter
+from locust.stats import stats_printer, stats_history
 
 from core.locust_script import MySeqTest
 from settings.config import TEST_SERVER_HOST
 from utilities.shared_resources import all_users_complete
+from utilities.write_locust_stats import write_locust_stats
 
 # Set the logging level for Locust
 logging.basicConfig(level=logging.INFO)
 
 
+# Used to generate the START and END log at the server
 def sys_perf_check(test_id, msg="", num_user=0):
     url = TEST_SERVER_HOST + f"sys_perf_check/{test_id}-{msg}/{num_user}/"
     requests.get(url)
+
+
+# Wait for users to finish
+def wait_for_users_to_finish(env, num_user):
+    """
+    Waits for all users to finish their tasks.
+    Stops the Locust runner when the semaphore value indicates all users are done.
+
+    :param env: Locust Environment instance.
+    :param num_user: Total number of users to wait for.
+    """
+    while True:
+        current_value = all_users_complete.value
+        print(f"Waiting for users. Current semaphore value: {current_value}")
+        if current_value >= num_user:
+            print("All users completed. Stopping Locust.")
+            break
+
+        gevent.sleep(2)
+
+    env.runner.quit()
 
 
 def performance_test(num_user, ramp_up, test_id):
@@ -26,62 +49,36 @@ def performance_test(num_user, ramp_up, test_id):
     # Calculate the user rate
     rate = ceil(num_user * ramp_up)
 
-    # Define the directory for CSV output
+    # Define the directory for CSV output of locust stats
     csv_output_dir = f"{test_id}"
     os.makedirs(csv_output_dir, exist_ok=True)
 
-    # Custom CSV filenames
-    stats_csv_path = os.path.join(csv_output_dir, f"{num_user}")
-    history_csv_path = os.path.join(csv_output_dir, f"{num_user}")
+    stats_csv_path = os.path.join(csv_output_dir, f"{num_user}_stats.csv")
 
-    # Create Locust environment with the PerfCheck class
+    # Create Locust Environment
     env = Environment(user_classes=[MySeqTest])
     env.create_local_runner()
 
-    # Define percentiles to report
-    percentiles_to_report = [50, 90, 95, 99]
-
-    # Set up CSV writers
-    StatsCSVFileWriter(
-        env.stats,
-        base_filepath=stats_csv_path,
-        percentiles_to_report=percentiles_to_report
-    )
-    StatsCSVFileWriter(
-        env.stats,
-        base_filepath=history_csv_path,
-        full_history=True,
-        percentiles_to_report=percentiles_to_report
-    )
-
-    # Start stats printing in the console and history tracking
+    # Start Locust Test
     printer_greenlet = gevent.spawn(stats_printer(env.stats))
 
     # Set up stats history tracking
     gevent.spawn(stats_history, env.runner)
 
-    # Track all users completion
-    def wait_for_users_to_finish():
-        while True:
-            current_value = all_users_complete.value
-            print(f"Waiting for users. Current semaphore value: {current_value}")
-            if current_value >= num_user:
-                print("All users completed. Stopping Locust.")
-                env.runner.quit()
-                break
-
-            gevent.sleep(2)
-
-    gevent.spawn(wait_for_users_to_finish)
+    # Set up a thread to track user completion
+    gevent.spawn(wait_for_users_to_finish, env, num_user)
 
     # Start the Locust test with the specified number of users and spawn rate
     env.runner.start(user_count=num_user, spawn_rate=rate)
 
-    # Run the test
+    # Locust Test Completes
     env.runner.greenlet.join()
 
     # Forcefully kill the stats printer greenlet
     printer_greenlet.kill()
+
+    # Manually write stats after test completion
+    write_locust_stats(env, stats_csv_path)
 
     # Generate the END log in the server
     sys_perf_check(test_id, "END")
